7599c9542c15428fe977ceda225b205bf2f8b0f1
python_ <theoreticallytrash.py@gmail.com>
2025-01-22 18:42:27 +0200

    Update character_body_2d.gd
    improve movement

--- scenes/entities/character_body_2d.gd
+++ scenes/entities/character_body_2d.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const tile_size := 32
+var moving := false
 var input_dir
 
 func _physics_process(delta: float) -> void:
@@ -8,12 +10,32 @@ func _physics_process(delta: float) -> void:
 	# TODO: Use InputMap
 	if Input.is_action_pressed("ui_down"):
 		input_dir = Vector2(0, 1)
+		move()
 	elif Input.is_action_pressed("ui_up"):
 		input_dir = Vector2(0, -1)
+		move()
 	elif Input.is_action_pressed("ui_right"):
 		input_dir = Vector2(1, 0)
+		move()
 	elif Input.is_action_pressed("ui_left"):
 		input_dir = Vector2(-1, 0)
+		move()
 	
-	velocity = input_dir * 100000 * delta
 	move_and_slide()
+
+
+func move():
+	if input_dir:
+		if moving == false:
+			moving = true
+			var tween = create_tween()
+
+			tween.tween_property(
+				self, "position", position + input_dir * tile_size, 0.1
+				)
+
+			tween.tween_callback(move_false)
+
+
+func move_false():
+	moving = false
