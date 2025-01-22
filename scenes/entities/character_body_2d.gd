21661cfdd8d8f92b338e22645cf6e63832b24461
python_ <theoreticallytrash.py@gmail.com>
2025-01-22 19:22:43 +0200

    get halfway through a tutorial and give up for the day

--- scenes/entities/character_body_2d.gd
+++ scenes/entities/character_body_2d.gd
@@ -1,41 +1,27 @@
 extends CharacterBody2D
 
-const tile_size := 32
-var moving := false
-var input_dir
+@onready var tile_map_layer = $"../TileMapLayer"
 
-func _physics_process(delta: float) -> void:
-	input_dir = Vector2.ZERO
-
-	# TODO: Use InputMap
-	if Input.is_action_pressed("ui_down"):
-		input_dir = Vector2(0, 1)
-		move()
-	elif Input.is_action_pressed("ui_up"):
-		input_dir = Vector2(0, -1)
-		move()
-	elif Input.is_action_pressed("ui_right"):
-		input_dir = Vector2(1, 0)
-		move()
-	elif Input.is_action_pressed("ui_left"):
-		input_dir = Vector2(-1, 0)
-		move()
-	
-	move_and_slide()
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("up"):
+		move(Vector2.UP)
+	elif Input.is_action_pressed("down"):
+		move(Vector2.DOWN)
+	elif Input.is_action_pressed("left"):
+		move(Vector2.LEFT)
+	elif Input.is_action_pressed("right"):
+		move(Vector2.RIGHT)
 
 
-func move():
-	if input_dir:
-		if moving == false:
-			moving = true
-			var tween = create_tween()
+func move(direction: Vector2):
+	# Get current tile Vector2i
+	var current_tile: Vector2i = tile_map_layer.local_to_map(global_position)
+	# Get target tile Vector2i
+	var target_tile: Vector2i = Vector2i(
+		current_tile.x + direction.x,
+		current_tile.y + direction.y,
+	)
+	print(current_tile, target_tile)
+	# Get custom data layer for target tile
 
-			tween.tween_property(
-				self, "position", position + input_dir * tile_size, 0.1
-				)
-
-			tween.tween_callback(move_false)
-
-
-func move_false():
-	moving = false
+	# Move player
